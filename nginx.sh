6773ec6a6e90887292699e2175d27d7c1c7f81b6
Dave Burke <dburke84@gmail.com>
2016-12-04 14:03:16 -0600

    Mount an init script

--- nginx.sh
+++ nginx.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 IMAGE=nginx:alpine
+NAME=nginx-proxy
 
-docker pull ${IMAGE}
-docker rm -f nginx
-docker run --detach --restart always --name nginx \
+here="$(cd $(dirname ${0}); pwd)"
+
+docker pull "${IMAGE}"
+docker rm -f "${NAME}"
+docker run \
+	--name "${NAME}" \
+	--restart always \
 	-p 80:80 \
-	-v /mnt/storage/docker/nginx/static:/usr/share/nginx/html \
-	-v /mnt/storage/docker/nginx/conf.d:/etc/nginx/conf.d \
-	${IMAGE}
+	-v "${here}/install":"/opt/${NAME}/":ro \
+	${IMAGE} \
+	"/opt/${NAME}/init.sh"
 
