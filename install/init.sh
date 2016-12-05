63953faba5a23a309f0a115eb977059c9aadea0f
Dave Burke <dburke84@gmail.com>
2016-12-04 20:29:10 -0600

    Generate hard coded config file

--- install/init.sh
+++ install/init.sh
@@ -1,4 +1,38 @@
 #!/bin/sh
 
+write_config() {
+	local subdomain="${1}"
+	local backend="${2}"
+	cat >> /etc/nginx/conf.d/"${subdomain}".conf <<-EOF
+	server {
+		listen 80;
+		server_name ${subdomain}.${HOSTNAME};
+		location / {
+			proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+			proxy_set_header X-Forwarded-Host \$host;
+			proxy_set_header X-Forwarded-Server \$host;
+			proxy_set_header X-Forwarded-Proto http;
+			proxy_pass ${backend};
+			proxy_set_header Host \$host;
+		}
+	}
+	EOF
+}
+
+cat > /etc/nginx/conf.d/default.conf <<-EOF
+server {
+	listen 80;
+	server_name ${HOSTNAME};
+
+	location / {
+		root /usr/share/nginx/html;
+		try_files \$uri \$uri/ =404;
+		autoindex on;
+	}
+
+}
+EOF
+
+write_config "print" "http://192.168.2.150:631"
 nginx -g "daemon off;"
 
